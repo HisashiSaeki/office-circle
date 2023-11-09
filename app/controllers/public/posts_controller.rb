50e648c97cf78a82fdd729d37d2e057f508a3d64
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-11-09 04:29:40 +0000

    Add _header.html.erb to application.html.erb

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,38 @@
 class Public::PostsController < ApplicationController
+  
+  def new
+    
+    
+  end
+  
+  def create
+    
+  end
+  
+  def index
+    
+  end
+  
+  def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :body, :employee_id, :is_published)
+  end
+  
 end
