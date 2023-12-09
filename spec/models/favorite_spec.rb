c308c42ae545dcc7e176376895fae87603cb4e13
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-12-09 14:45:56 +0000

    Add RSpec tests for favorite model

---
+++ spec/models/favorite_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Favorite, type: :model do
+  let!(:employee) { create(:employee) }
+  let!(:article) { create(:article) }
+  describe "バリデーションテスト" do
+    it "同じ記事に同じ社員がいいねはできない" do
+      Favorite.create(employee_id: employee.id, article_id: article.id)
+      expect(Favorite.create(employee_id: employee.id, article_id: article.id)).to be_invalid
+    end
+  end
+end
