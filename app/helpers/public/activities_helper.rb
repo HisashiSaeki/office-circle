e3c759aa32b7409cde7e830cbd761c91301bdaa8
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2024-01-09 08:31:57 +0000

    Remove transition_path(activity) method

--- app/helpers/public/activities_helper.rb
+++ app/helpers/public/activities_helper.rb
@@ -1,15 +1,2 @@
 module Public::ActivitiesHelper
-
-  def transition_path(activity)
-    case activity.action_type.to_sym
-    when :commented_to_own_article
-      article_path(activity.subject.article, anchor: "comment-#{activity.subject.id}")
-    when :liked_to_own_article
-      article_path(activity.subject.article)
-    when :noticed_to_participating_group
-      group_path(activity.subject.group)
-    end
-  end
-
-
 end
